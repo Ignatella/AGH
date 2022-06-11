19d5d0b2200f5c826f96b88bb8711fe6cb3565fd
Ignatella <ignatellasecond@gmail.com>
2022-06-11 14:09:09 +0200

    Fix: SEM4 Scala labs 9

--- SEM_4/FP/ScalaLabs/src/Lab9/Ex1/Ex1.scala
+++ SEM_4/FP/ScalaLabs/src/Lab9/Ex1/Ex1.scala
@@ -1,26 +1,31 @@
 package Lab9.Ex1
 
-case class Value(v: Double) {
+abstract class Action
+
+case class Value(v: Double) extends Action {
   override def toString: String = "%.4f".format(v)
 }
 
-object Sum {
-  def apply(v1: Value, v2: Value): Value = Value(v1.v + v2.v)
+case class Sum(v1: Action, v2: Action) extends Action
+
+case class Subtract(v1: Action, v2: Action) extends Action
+
+case class Multiply(v1: Action, v2: Action) extends Action
+
+case class Divide(v1: Action, v2: Action) extends Action
+
+object Evaluate {
+  def apply(act: Action): Value = act match {
+    case Value(v) => Value(v)
+    case Sum(v1, v2) => Value(Evaluate(v1).v + Evaluate(v2).v)
+    case Subtract(v1, v2) => Value(Evaluate(v1).v - Evaluate(v2).v)
+    case Multiply(v1, v2) => Value(Evaluate(v1).v * Evaluate(v2).v)
+    case Divide(v1, v2) => Value(Evaluate(v1).v / Evaluate(v2).v)
+  }
 }
 
-object Subtract {
-  def apply(v1: Value, v2: Value): Value = Value(v1.v - v2.v)
-}
-
-object Multiply {
-  def apply(v1: Value, v2: Value): Value = Value(v1.v * v2.v)
-}
-
-object Divide {
-  def apply(v1: Value, v2: Value): Value = Value(v1.v / v2.v)
-}
 
 object Ex1 extends App {
   val result = Divide(Subtract(Multiply(Sum(Value(1), Value(2)), Value(3)), Value(7)), Value(3))
-  println(result)
+  println(Evaluate(result))
 }
